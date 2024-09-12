302629df69ca6b4269df99314c6cfb5b1412edec
Marien Fieret <marien.fieretdv@gmail.com>
2024-09-12 07:55:57 +0200

    change base url

--- src/utils/customFetch.js
+++ src/utils/customFetch.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const customFetch = axios.create({
-	// baseURL: "https://ordersysteem-api.onrender.com/api/v1",
-	baseURL: "http://localhost:5000/api/v1",
+	baseURL: "https://ordersysteem-api.onrender.com/api/v1",
+	// baseURL: "http://localhost:5000/api/v1",
 	withCredentials: true,
 	headers: {
 		"Content-type": "application/json",
